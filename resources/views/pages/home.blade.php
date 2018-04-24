f22b02b160620c762e33fc622a68250e7a7cf2ec
Tyler Reed <tylernathanreed@gmail.com>
2018-04-24 14:12:59 -0500

    + Added services section

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -127,4 +127,66 @@
         ]
     ])
 
+    <section id="services" class="light-gray-bg pv-30 clearfix">
+        <div class="container">
+          <div class="row justify-content-lg-center">
+            <div class="col-lg-8">
+              <h2 class="text-center">Main <strong>Services</strong></h2>
+              <div class="separator"></div>
+              <p class="large text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam voluptas facere vero ex tempora saepe perspiciatis ducimus sequi animi.</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-4">
+              <div class="image-box style-2 mb-20">
+                <div class="overlay-container overlay-visible">
+                  <img src="{{ asset('media/images/service-1.jpg') }}" alt="">
+                  <a href="{{ asset('media/images/service-1.jpg') }}" class="overlay-link popup-img"><i class="fa fa-plus"></i></a>
+                  <div class="overlay-bottom hidden-sm-down">
+                    <div class="text">
+                      <p class="lead margin-clear text-left">Service One</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="body padding-horizontal-clear">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam atque ipsam nihil, adipisci rem minus? Voluptatem distinctio laborum porro aspernatur.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="image-box style-2 mb-20">
+                <div class="overlay-container overlay-visible">
+                  <img src="{{ asset('media/images/service-2.jpg') }}" alt="">
+                  <a href="{{ asset('media/images/service-2.jpg') }}" class="overlay-link popup-img"><i class="fa fa-plus"></i></a>
+                  <div class="overlay-bottom hidden-sm-down">
+                    <div class="text">
+                      <p class="lead margin-clear text-left">Service Two</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="body padding-horizontal-clear">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam atque ipsam nihil, adipisci rem minus? Voluptatem distinctio laborum porro aspernatur.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="image-box style-2 mb-20">
+                <div class="overlay-container overlay-visible">
+                  <img src="{{ asset('media/images/service-3.jpg') }}" alt="">
+                  <a href="{{ asset('media/images/service-3.jpg') }}" class="overlay-link popup-img"><i class="fa fa-plus"></i></a>
+                  <div class="overlay-bottom hidden-sm-down">
+                    <div class="text">
+                      <p class="lead margin-clear text-left">Service Three</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="body padding-horizontal-clear">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam atque ipsam nihil, adipisci rem minus? Voluptatem distinctio laborum porro aspernatur.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
 @endsection
