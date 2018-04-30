922a5eec81233eca8d4e3ead879f0a35b1a9c468
Tyler Reed <tylernathanreed@gmail.com>
2018-04-30 13:02:43 -0500

    ~ Fixed slideshow

--- resources/views/partials/slideshow.blade.php
+++ resources/views/partials/slideshow.blade.php
@@ -4,131 +4,186 @@
     
     <!-- slider revolution start -->
     <!-- ================ -->
-    <div class="slider-banner-container">
-        <div class="slider-banner-fullwidth">
+    <div class="slider-revolution-5-container">
+        <div id="slider-banner-fullwidth" class="slider-banner-fullwidth rev_slider" data-version="5.0" style="height: 39.485418vw; max-height: 450px">
             <ul class="slides">
+
                 <!-- slide 1 start -->
                 <!-- ================ -->
-                <li data-transition="fade" data-slotamount="1" data-masterspeed="500" data-saveperformance="on" data-title="Next Generation Template">
+                <li data-transition="fade" data-slotamount="default" data-masterspeed="default" data-title="E and M Home Service">
                 
-                <!-- main image -->
-                <img src="{{ asset('media/images/slideshow-slide-1.jpg') }}" alt="slidebg1" data-bgposition="center top"  data-bgrepeat="no-repeat" data-bgfit="cover">
+                    <!-- main image -->
+                    <img src="{{ asset('media/images/slideshow-slide-1.jpg') }}" alt="slidebg1" data-bgposition="center top"  data-bgrepeat="no-repeat" data-bgfit="cover" class="rev-slidebg">
 
-                <!-- Transparent Background -->
-                <div class="tp-caption dark-translucent-bg"
-                    data-x="center"
-                    data-y="bottom"
-                    data-speed="600"
-                    data-start="0">
-                </div>
+                    <!-- Transparent Background -->
+                    <div class="tp-caption dark-translucent-bg"
+                        data-x="center"
+                        data-y="bottom"
+                        data-start="0"
+                        data-transform_idle="o:1;"
+                        data-transform_in="o:0;s:600;e:Power2.easeInOut;"
+                        data-transform_out="o:0;s:600;">
+                    </div>
 
-                <!-- LAYER NR. 1 -->
-                <div class="tp-caption sfb fadeout large_white"
-                    data-x="left"
-                    data-y="110"
-                    data-speed="500"
-                    data-start="1000"
-                    data-easing="easeOutQuad">E&amp;M Home Service
-                </div>  
+                    <!-- LAYER NR. 1 -->
+                    <div class="tp-caption large_white"
+                      data-x="left"
+                      data-y="110"
+                      data-start="500"
+                      data-transform_idle="o:1;"
+                      data-transform_in="y:-110;o:0;s:1000;e:Power2.easeInOut;"
+                      data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;"
+                      data-mask_in="x:0px;y:[100%];s:inherit;e:inherit;"
+                      data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;">
+                        E&amp;M Home Service
+                    </div>
 
-                <!-- LAYER NR. 2 -->
-                <div class="tp-caption sfb fadeout large_white tp-resizeme hidden-xs"
-                    data-x="left"
-                    data-y="170"
-                    data-speed="500"
-                    data-start="1300"
-                    data-easing="easeOutQuad"><div class="separator-2 light"></div>
-                </div>  
+                    <!-- LAYER NR. 2 -->
+                    <div class="tp-caption large_white tp-resizeme"
+                        data-x="left"
+                        data-y="170"
+                        data-start="750"
+                        data-transform_idle="o:1;"
+                        data-transform_in="o:0;s:1000;e:Power4.easeInOut;">
+                        <div class="separator-2 light"></div>
+                    </div>  
 
-                <!-- LAYER NR. 3 -->
-                <div class="tp-caption sfb fadeout medium_white hidden-xs"
-                    data-x="left"
-                    data-y="180"
-                    data-speed="500"
-                    data-start="1300"
-                    data-easing="easeOutQuad"
-                    data-endspeed="600">Your #1 source for friendly in-home appliance repair
-                </div>
+                    <!-- LAYER NR. 3 -->
+                    <div class="tp-caption medium_white"
+                        data-x="left"
+                        data-y="180"
+                        data-start="1200"
+                        data-transform_idle="o:1;"
+                        data-transform_in="y:[100%];s:1000;e:Power2.easeInOut;"
+                        data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;"
+                        data-mask_in="x:0px;y:0px;s:inherit;e:inherit;"
+                        data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;">
+                        Your #1 source for friendly in-home appliance repair
+                    </div>
 
-                <!-- LAYER NR. 4 -->
-                <div class="tp-caption sfb fadeout small_white"
-                    data-x="left"
-                    data-y="260"
-                    data-speed="500"
-                    data-start="1600"
-                    data-easing="easeOutQuad"
-                    data-endspeed="600">
-                    <a href="#" class="btn radius-50 btn-dark btn-animated-left">
-                        <i class="fa fa-usd"></i> See Pricing
-                    </a>
-                    <span class="pl-5 pr-5">or</span>
-                    <a href="page-contact.html" class="btn radius-50 btn-default btn-animated-left">
-                        <i class="fa fa-truck"></i> Submit a Service Request
-                    </a>
-                </div>
+                    <!-- LAYER NR. 4 -->
+                    <div class="tp-caption sfb fadeout small_white"
+                        data-x="left"
+                        data-y="260"
+                        data-start="1800"
+                        data-transform_idle="o:1;"
+                        data-transform_in="y:[100%];o:0;s:1000;e:Power2.easeInOut;"
+                        data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;"
+                        data-mask_in="x:0px;y:[100%];s:inherit;e:inherit;"
+                        data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;">
+                        <a href="#" class="btn radius-50 btn-dark btn-animated-left">
+                            <i class="fa fa-usd"></i> See Pricing
+                        </a>
+                        <span class="pl-5 pr-5">or</span>
+                        <a href="page-contact.html" class="btn radius-50 btn-default btn-animated-left">
+                            <i class="fa fa-truck"></i> Submit a Service Request
+                        </a>
+                    </div>
 
+                    <!-- LAYER NR. 5 -->
+                    <div class="tp-caption"
+                      data-x="center"
+                      data-y="bottom"
+                      data-voffset="10"
+                      data-start="2200"
+                      data-transform_idle="o:1;"
+                      data-transform_in="y:[100%];o:0;s:1000;e:Power4.easeInOut;"
+                      data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;"
+                      data-mask_in="x:0px;y:[100%];s:inherit;e:inherit;"
+                      data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;">
+                        <a href="#page-start" class="btn btn-lg moving smooth-scroll">
+                            <i class="fa fa-angle-down"></i><i class="fa fa-angle-down"></i><i class="fa fa-angle-down"></i>
+                        </a>
+                    </div>
                 </li>
                 <!-- slide 1 end -->
 
                 <!-- slide 2 start -->
                 <!-- ================ -->
-                <li data-transition="fade" data-slotamount="1" data-masterspeed="500" data-saveperformance="on" data-title="Premium Bootstrap Template">
+                <li data-transition="fade" data-slotamount="default" data-masterspeed="default" data-title="Service Call Fee waived with Repair">
                 
-                <!-- main image -->
-                <img src="{{ asset('media/images/slideshow-slide-2.jpg') }}" alt="slidebg2" data-bgposition="center top"  data-bgrepeat="no-repeat" data-bgfit="cover">
+                    <!-- main image -->
+                    <img src="{{ asset('media/images/slideshow-slide-2.jpg') }}" alt="slidebg2" data-bgposition="center top"  data-bgrepeat="no-repeat" data-bgfit="cover" class="rev-slidebg">
 
-                <!-- Transparent Background -->
-                <div class="tp-caption dark-translucent-bg"
-                    data-x="center"
-                    data-y="bottom"
-                    data-speed="600"
-                    data-start="0">
-                </div>
+                    <!-- Transparent Background -->
+                    <div class="tp-caption dark-translucent-bg"
+                        data-x="center"
+                        data-y="center"
+                        data-start="0"
+                        data-transform_in="o:0;s:600;e:Power2.easeInOut;"
+                        data-transform_out="o:0;s:600;"
+                        data-width="5000"
+                        data-height="5000">
+                    </div>
 
-                <!-- LAYER NR. 1 -->
-                <div class="tp-caption sfb fadeout text-center large_white"
-                    data-x="center"
-                    data-y="110"
-                    data-speed="500"
-                    data-start="1000"
-                    data-easing="easeOutQuad">Service Call Fee waived with Repair
-                </div>  
+                    <!-- LAYER NR. 1 -->
+                    <div class="tp-caption text-center large_white"
+                        data-x="center"
+                        data-y="110"
+                        data-start="500"
+                        data-transform_in="y:-110;o:0;s:1000;e:Power2.easeInOut;"
+                        data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;"
+                        data-mask_in="x:0px;y:[100%];s:inherit;e:inherit;"
+                        data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;">
+                        Service Call Fee waived with Repair
+                    </div>  
 
-                <!-- LAYER NR. 2 -->
-                <div class="tp-caption sfb fadeout text-center large_white tp-resizeme hidden-xs"
-                    data-x="center"
-                    data-y="150"
-                    data-speed="500"
-                    data-start="1300"
-                    data-easing="easeOutQuad"><div class="separator light"></div>
-                </div>  
+                    <!-- LAYER NR. 2 -->
+                    <div class="tp-caption large_white tp-resizeme"
+                        data-x="center"
+                        data-y="170"
+                        data-start="750"
+                        data-transform_in="o:0;s:1000;e:Power4.easeInOut;">
+                        <div class="separator-3 light"></div>
+                    </div>
 
-                <!-- LAYER NR. 3 -->
-                <div class="tp-caption sfb fadeout medium_white text-center hidden-xs"
-                    data-x="center"
-                    data-y="180"
-                    data-speed="500"
-                    data-start="1300"
-                    data-easing="easeOutQuad"
-                    data-endspeed="600">We will not nickle and dime you to drive up your bill
-                </div>
+                    <!-- LAYER NR. 3 -->
+                    <div class="tp-caption medium_white"
+                        data-x="center"
+                        data-y="180"
+                        data-start="1200"
+                        data-transform_idle="o:1;"
+                        data-transform_in="y:[100%];s:1000;e:Power2.easeInOut;"
+                        data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;"
+                        data-mask_in="x:0px;y:0px;s:inherit;e:inherit;"
+                        data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;">
+                        We will not nickle and dime you to drive up your bill
+                    </div>
 
-                <!-- LAYER NR. 4 -->
-                <div class="tp-caption sfb fadeout small_white text-center"
-                    data-x="center"
-                    data-y="260"
-                    data-speed="500"
-                    data-start="1600"
-                    data-easing="easeOutQuad"
-                    data-endspeed="600">
-                    <a href="#" class="btn radius-50 btn-dark btn-animated-left">
-                        <i class="fa fa-usd"></i> See Pricing
-                    </a>
-                    <span class="pl-5 pr-5">or</span>
-                    <a href="page-contact.html" class="btn radius-50 btn-default btn-animated-left">
-                        <i class="fa fa-truck"></i> Submit a Service Request
-                    </a>
-                </div>
+                    <!-- LAYER NR. 4 -->
+                    <div class="tp-caption small_white"
+                        data-x="center"
+                        data-y="260"
+                        data-start="1800"
+                        data-transform_idle="o:1;"
+                        data-transform_in="y:[100%];o:0;s:1000;e:Power4.easeInOut;"
+                        data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;"
+                        data-mask_in="x:0px;y:[100%];s:inherit;e:inherit;"
+                        data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;">
+                        <a href="#" class="btn radius-50 btn-dark btn-animated-left">
+                            <i class="fa fa-usd"></i> See Pricing
+                        </a>
+                        <span class="pl-5 pr-5">or</span>
+                        <a href="page-contact.html" class="btn radius-50 btn-default btn-animated-left">
+                            <i class="fa fa-truck"></i> Submit a Service Request
+                        </a>
+                    </div>
+
+                    <!-- LAYER NR. 5 -->
+                    <div class="tp-caption"
+                        data-x="center"
+                        data-y="bottom"
+                        data-voffset="10"
+                        data-start="2200"
+                        data-transform_idle="o:1;"
+                        data-transform_in="y:[100%];o:0;s:1000;e:Power4.easeInOut;"
+                        data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;"
+                        data-mask_in="x:0px;y:[100%];s:inherit;e:inherit;"
+                        data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;">
+                        <a href="#page-start" class="btn btn-lg moving smooth-scroll">
+                            <i class="fa fa-angle-down"></i><i class="fa fa-angle-down"></i><i class="fa fa-angle-down"></i>
+                        </a>
+                    </div>
 
                 </li>
                 <!-- slide 2 end -->
